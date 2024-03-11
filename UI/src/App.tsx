559b576177e279eda57a610285b8f02785d7f82b
Chonakan <cshuhand@gmail.com>
2024-03-12 00:58:58 +0700

    nav bar

---
+++ UI/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Nav from './nav/nav'
+
+function App() {
+
+  return (
+    <>
+      <div className='max-h-screen flex flex-col'>
+        <Nav />
+        {/* <div>2</div> */}
+      </div>
+     
+    </>
+  )
+}
+
+export default App
